cb5364b0577a8a06cda759e4a9c2097f6e2bc7a9
sahasra101 <ajaykiri7@gmail.com>
2019-11-20 23:32:43 -0500

    configured server to use any available PORT

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 var path = require("path");
 const app = express();
+var PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World - The server is working!!')
 })
  
-app.listen(3000);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: "+ PORT);
+});
 
