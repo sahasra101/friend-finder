88c511a3b91664a7ec83e08232f79d770228e829
sahasra101 <ajaykiri7@gmail.com>
2019-11-21 01:12:38 -0500

    set up routing files and survey file

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = function(app) {
+var friends = [
+    {
+    name: "Jason",
+    photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/04/02/02-jason-statham-chatroom.w330.h412.jpg",
+    scores: [3,
+        2,
+        1,
+        5,
+        3,
+        1,
+        5,
+        2,
+        4,
+        1]
+}
+];
+};
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const express = require('express');
+var express = require('express');
 var path = require("path");
-const app = express();
+var app = express();
 var PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- 
-app.get('/', function (req, res) {
-  res.send('Hello World - The server is working!!')
-})
- 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: "+ PORT);
+
+require('./app/data/friends')(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// app.get('/home', function (req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/home.html"));
+// })
+
+// app.get('/survey', function (req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+// })
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
 
