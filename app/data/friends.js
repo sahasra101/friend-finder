78964ef6c5b8cc60927b7ae1f033a76ee7bd605a
sahasra101 <ajaykiri7@gmail.com>
2019-11-21 20:35:13 -0500

    set up calculation for match and ajax call for match

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,97 @@ var path = require("path");
 
 var friends = [
     {
-    name: "Jason",
-    photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/04/02/02-jason-statham-chatroom.w330.h412.jpg",
-    scores: [3,
-        2,
-        1,
-        5,
-        3,
-        1,
-        5,
-        2,
-        4,
-        1]
-}
+        name: "Jason",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/04/02/02-jason-statham-chatroom.w330.h412.jpg",
+        scores: [3,
+            2,
+            1,
+            5,
+            3,
+            1,
+            5,
+            2,
+            4,
+            1]
+    }, {
+        name: "Billy",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/61C8gGw4hrL._SX355_.jpg",
+        scores: [5,
+            3,
+            2,
+            4,
+            3,
+            2,
+            2,
+            1,
+            3,
+            2]
+    }, {
+        name: "Albert",
+        photo: "https://vignette.wikia.nocookie.net/scratchpad-iii/images/0/00/It%27s_Fat_Albert.png/revision/latest?cb=20190201151747",
+        scores: [5,
+            2,
+            4,
+            1,
+            5,
+            2,
+            4,
+            3,
+            5,
+            2]
+    }, {
+        name: "Russell",
+        photo: "https://sports.mynorthwest.com/wp-content/uploads/2019/11/russell-wilson-getty-run-620.jpg",
+        scores: [5,
+            2,
+            4,
+            1,
+            5,
+            2,
+            4,
+            3,
+            5,
+            2]
+    }, {
+        name: "Rafael",
+        photo: "https://si.wsj.net/public/resources/images/BN-TU959_36xG7_OR_20170611105935.jpg?width=620&height=413",
+        scores: [1,
+            3,
+            4,
+            5,
+            2,
+            5,
+            1,
+            3,
+            5,
+            1]
+    }, {
+        name: "Stefani",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Lady_Gaga_interview_2016.jpg",
+        scores: [1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1]
+    }, {
+        name: "Grumpy",
+        photo: "https://media.boingboing.net/wp-content/uploads/2019/02/trump.jpeg",
+        scores: [5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5]
+    }
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,50 @@ var path = require("path");
 
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-      });
 
-    app.post("/api/friends", function(req, res) {
+module.exports = function (app) {
+    var match;
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    
+    app.post("/api/friends", function (req, res) {
+
+        function compScores(arr1, arr2) {
+            var tempScore = 0;
+            for (var i = 0; i < 10; i++) {
+                tempScore += Math.abs(parseInt(arr1[i]) - parseInt(arr2[i]));
+            }
+            return tempScore;
+        }
+        var closestMatch = 40;
+        for (var i = 0; i < friends.length; i++) {
+            tempScore = compScores(req.body.scores, friends[i].scores)
+            if (tempScore < closestMatch) {
+                // this is the new match
+                closestMatch = tempScore;
+                matchedindex = i;
+            }
+        }
+        console.log(closestMatch);
+        console.log(friends[matchedindex].name);
+        console.log(friends[matchedindex].photo);
+        match = {
+            name:friends[matchedindex].name,
+            photo:friends[matchedindex].photo
+        }
+        // res.json(match);
+        
+      
+
         friends.push(req.body);
-    });  
+
+    });
+
+    app.get("/api/match", function (req, res) {
+        res.json(match);
+
+    });
+
 };
